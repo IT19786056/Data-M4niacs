7db431377a5a0e76573ce28c7171ab49cb368a52
IT19785752 <59859872+IT19785752@users.noreply.github.com>
2020-05-06 21:32:06 +0530

    Manage undergraduate degree prog

---
+++ Undergraduate_degree.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+/**
+ *
+ * @author budaramal
+ */
+public class Undergraduate_degree
+{
+ 
+   private int Degree_ID;
+   private String degree_Name;
+   private String partner_uni;
+   private int Degree_Fee;
+   private int Degree_Period;
+   private int Degree_GPA;
+   private String Entity_Reqirements;
+   
+   public void AddNewDegree()
+   {
+       
+   }
+   
+    public void UpdateDegreeDetail()
+    {
+        
+    }
+    
+    public void DeleteDegree()
+    {
+        
+    }
+ 
+        
+}
