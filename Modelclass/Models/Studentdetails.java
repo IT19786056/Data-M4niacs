55dbde336eb3633083b02c276eae63705cf27389
IT19786056 <59858097+IT19786056@users.noreply.github.com>
2020-05-06 19:28:03 +0530

    Enrollment of Student Details

---
+++ Modelclass/Models/Studentdetails.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Jason Lawrence
+ */
+public class Studentdetails 
+{
+   public String name;
+   public String Address;
+   public String National_ID;
+   public String DOB;
+   public String EMAIIL;
+   public int telephone;
+   public String gender;
+   
+   public void GetstudentInformation()
+   {
+       
+   }
+}
