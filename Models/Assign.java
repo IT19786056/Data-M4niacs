952055b405eb0c15c37c9f2bcd98affa5594772c
it19223940 <59859816+it19223940@users.noreply.github.com>
2020-05-06 20:42:44 +0530

    Add files via upload

---
+++ Models/Assign.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Hasheena
+ */
+public class Assign {
+    
+    public String task;
+    public Assign( String task){
+        
+        this.task=task; 
+
+    }
+    
+    public String task(String empId,String tas)
+    {
+        return task;
+    }
+}
